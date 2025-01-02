57fe957edff4d8d369e3a22bca37ca139b87e0b3
txwdzxq <txwdzxq@sina.com>
2025-01-02 07:46:37 +0000

    update dist content from compiler

---
+++ functions/file/list.ts
@@ -0,0 +1,22 @@
+interface Env {
+  img_url: KVNamespace;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const {
+    // request,
+    env,
+    // params,
+    // waitUntil,
+    // next,
+    // data,
+  } = context;
+  console.log(env)
+  const img_url_list = env.img_url.list().catch(e => {
+    console.log(e);
+    return [{"name": "e"},{"name": "rr"},{"name": "or"}];
+  });
+  const img_url_keys = JSON.stringify((await img_url_list).keys);
+  console.log(img_url_keys)
+  return new Response(img_url_keys);
+};
