9cc000f36975217d5432287341ea2d7898094ba8
txwdzxq <txwdzxq@sina.com>
2025-01-03 01:24:50 +0800

    delete pic

--- src/pages/ManageImage.vue
+++ src/pages/ManageImage.vue
@@ -62,17 +62,18 @@ function copyImageUrl(img_path: string) {
 }
 
 function deleteImage(event: MouseEvent, img_path: string) {
-  console.log('Image deleted');
-  axios.delete('/file/delete/' + img_path)
-    .then(res => {
-        const currentElement = event.currentTarget as HTMLDivElement;
-        const wrapDeleteButtonLine = currentElement.parentElement as HTMLDivElement;
-        const imageContainer = wrapDeleteButtonLine.parentElement as HTMLDivElement;
-        imageContainer.remove()
-        console.log(res);
-      }
-    )
-    .catch(e => console.log(e));
+  console.log('Image deleted',event.currentTarget);
+
+//   axios.delete('/file/delete/' + img_path)
+//     .then(res => {
+//         const currentElement = event.currentTarget as HTMLDivElement;
+//         const wrapDeleteButtonLine = currentElement.parentElement as HTMLDivElement;
+//         const imageContainer = wrapDeleteButtonLine.parentElement as HTMLDivElement;
+//         imageContainer.remove()
+//         console.log(res);
+//       }
+//     )
+//     .catch(e => console.log(e));
 }
 
 </script>
