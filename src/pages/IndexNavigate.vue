7de8e0fbd1bcb5b6813db63f41ffdd404e6f49db
txwdzxq <txwdzxq@sina.com>
2025-01-10 04:53:49 +0800

    upload ui

--- src/pages/IndexNavigate.vue
+++ src/pages/IndexNavigate.vue
@@ -9,11 +9,13 @@ const logoutTrigger = () => {
 <template>
   <div class="index-navigate">
     <div class="navigate-item">
+      <div class="spacer"></div>
       <RouterLink to="/"><span>上传图片</span></RouterLink>
       <RouterLink :to="{name: 'manage-image'}"><span>管理图片</span></RouterLink>
       <RouterLink :to="{name: 'test-image'}"><span>测试</span></RouterLink>
+      <div class="spacer"></div>
+      <button class="logout" ref="logout" @click="logoutTrigger"><span>Logout</span></button>
     </div>
-    <button class="logout" ref="logout" @click="logoutTrigger"><span>Logout</span></button>
   </div>
 </template>
 
@@ -23,16 +25,26 @@ const logoutTrigger = () => {
   height: 5vh;
   min-height: 60px;
   text-align: center;
-  justify-items: center;
+  align-items: center;
   font-size: xx-large;
 }
 
 .navigate-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex: auto;
   margin: auto;
 }
 
 .logout {
+  display: inline;
+  height: 50px;
   margin-left: 0;
+}
+
+.spacer {
+  flex: 1;
 }
 
 span {
