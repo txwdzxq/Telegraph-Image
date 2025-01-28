295cfc8bfa2a61d44176f0c7e30a31568095995c
txwdzxq <txwdzxq@sina.com>
2025-01-29 03:55:05 +0800

    test ai

--- functions/deepseed/test.ts
+++ functions/deepseed/test.ts
@@ -7,14 +7,14 @@ export const onRequest: PagesFunction = async (context) => {
   console.log(typeof request, typeof env, typeof params);
 
   const res_data: object[] = [];
-  // let question;
-  // new URL(request.url).searchParams.forEach((value, key) => {
-  //   if (key === 'q') {
-  //     question = {prompt: atob(value)};
-  //   }
-  // })
-  // const response = await env.AI.run('@cf/meta/llama-3-8b-instruct', question);
-  // res_data.push({inputs: question, response});
+  let question;
+  new URL(request.url).searchParams.forEach((value, key) => {
+    if (key === 'q') {
+      question = {prompt: atob(value)};
+    }
+  })
+  const response = await env.AI.run('@cf/meta/llama-3-8b-instruct', question);
+  res_data.push({inputs: question, response});
   res_data.push({url:request.url});
 
   return new Response(JSON.stringify(res_data),
