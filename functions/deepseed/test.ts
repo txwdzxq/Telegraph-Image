8df054c8263136aa77d043833b8ba2a8cd2515d0
txwdzxq <txwdzxq@sina.com>
2025-01-28 19:43:57 +0000

    update dist content from compiler

---
+++ functions/deepseed/test.ts
@@ -0,0 +1,25 @@
+export const onRequest: PagesFunction = async (context) => {
+  const {
+    request,
+    env,
+    params,
+  } = context;
+  console.log(typeof request, typeof env, typeof params);
+
+  const res_data: object[] = [];
+  let question;
+  new URL(request.url).searchParams.forEach((value, key) => {
+    if (key === 'q') {
+      question = {prompt: atob(value)};
+    }
+  })
+  const response = await env.AI.run('@cf/meta/llama-3-8b-instruct', question);
+  res_data.push({inputs: question, response});
+
+  return new Response(JSON.stringify(res_data),
+    {
+      headers: {'Content-Type': 'text/html'},
+      status: 200,
+    });
+};
+
