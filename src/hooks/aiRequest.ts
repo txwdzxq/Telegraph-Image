861019b9b18ea19a3fb024e44c8f3477e58f27ad
txwdzxq <txwdzxq@sina.com>
2025-01-29 23:23:43 +0800

    test ai

--- src/hooks/aiRequest.ts
+++ src/hooks/aiRequest.ts
@@ -4,7 +4,7 @@ export function aiRequest() {
   const query = async (url_path: string, prompt: string | undefined): Promise<string[]> => {
     if (prompt) {
       try {
-        const result = await axios.get(url_path + '/deepseed/test?q=' + atob(encodeURIComponent(prompt)), {headers: {'Content-Type': 'multipart/form-data'}});
+        const result = await axios.get(url_path + '/deepseed/test?q=' + btoa(encodeURIComponent(prompt)), {headers: {'Content-Type': 'multipart/form-data'}});
         return result.data;
       } catch (e) {
         console.log(e)
