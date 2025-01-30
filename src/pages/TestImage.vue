89e7db2a7570a8f6b243e33c80565747c0e5e22b
txwdzxq <txwdzxq@sina.com>
2025-01-31 03:20:58 +0800

    test ai

--- src/pages/TestImage.vue
+++ src/pages/TestImage.vue
@@ -4,22 +4,23 @@ import {aiRequest} from "@/hooks/aiRequest.ts";
 
 const {query} = aiRequest()
 
+interface dialogue {
+  id: string;
+  text: string;
+}
+
 const prompt = ref<HTMLInputElement>();
-const dialogues = ref<HTMLDivElement>();
 const window_location_origin = ref<string>(window.location.origin)
+const dialogues = ref<dialogue[]>([]);
 
 
 const commitPrompt = () => {
   if (prompt.value) {
-    const div_prompt = new HTMLDivElement();
-    div_prompt.innerText = prompt.value.value
-    dialogues.value?.appendChild(div_prompt)
+    dialogues.value.push({id: new Date().getTime().toString(), text: prompt.value.value});
   }
   query(window_location_origin.value, prompt.value?.value)
     .then(res => {
-      if (dialogues.value) {
-        dialogues.value.innerText = JSON.stringify(res);
-      }
+      dialogues.value.push({id: new Date().getTime().toString(), text: JSON.stringify(res)});
     });
 }
 
@@ -28,7 +29,7 @@ const commitPrompt = () => {
 
 <template>
   <input ref="prompt">
-  <div ref="dialogues"></div>
+  <div v-for="dialogue in dialogues" :key="dialogue.id">{{ dialogue.text }}</div>
   <button ref="commit-prompt" @click="commitPrompt">commit</button>
 </template>
 
