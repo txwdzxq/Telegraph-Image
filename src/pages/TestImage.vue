ab1977fddb0cf9f69b45d82b6785d323b5043ae5
txwdzxq <txwdzxq@sina.com>
2025-01-31 02:56:23 +0800

    test ai

--- src/pages/TestImage.vue
+++ src/pages/TestImage.vue
@@ -5,17 +5,22 @@ import {aiRequest} from "@/hooks/aiRequest.ts";
 const {query} = aiRequest()
 
 const prompt = ref<HTMLInputElement>();
-const result = ref<HTMLDivElement>();
+const dialogues = ref<HTMLDivElement>();
 const window_location_origin = ref<string>(window.location.origin)
 
 
 const commitPrompt = () => {
+  if (prompt.value) {
+    const div_prompt = new HTMLDivElement();
+    div_prompt.innerText = prompt.value.value
+    dialogues.value?.appendChild(div_prompt)
+  }
   query(window_location_origin.value, prompt.value?.value)
     .then(res => {
-      if (result.value) {
-        result.value.innerText = JSON.stringify(res);
+      if (dialogues.value) {
+        dialogues.value.innerText = JSON.stringify(res);
       }
-    })
+    });
 }
 
 
@@ -23,7 +28,7 @@ const commitPrompt = () => {
 
 <template>
   <input ref="prompt">
-  <div ref="result"></div>
+  <div ref="dialogues"></div>
   <button ref="commit-prompt" @click="commitPrompt">commit</button>
 </template>
 
