6b65e88c88519fb081d51efe67e2816161fbe8d3
txwdzxq <txwdzxq@sina.com>
2025-01-31 14:17:15 +0000

    update dist content from compiler

---
+++ functions/deepseed/question.ts
@@ -0,0 +1,32 @@
+import {Ai} from '@cloudflare/ai/dist/sdk.js'
+
+export interface Env {
+  AI: Ai;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const {
+    request,
+    env,
+    params,
+  } = context;
+  console.log(typeof request, typeof env, typeof params);
+
+  const res_data: object[] = [];
+  let question: object;
+  new URL(request.url).searchParams.forEach((value, key) => {
+    if (key === 'q') {
+      question = {prompt: decodeURIComponent(atob(value))};
+    }
+  })
+  const response = await env.AI.run('@cf/deepseek-ai/deepseek-r1-distill-qwen-32b', question);
+  res_data.push({inputs: question, response: response});
+  // res_data.push({url:request.url});
+
+  return new Response(JSON.stringify(res_data),
+    {
+      headers: {'Content-Type': 'text/html'},
+      status: 200,
+    });
+};
+
